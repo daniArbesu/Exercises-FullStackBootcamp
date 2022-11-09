28411d389101715a543087cf5b65c60ad8dfdf41
daniArbesu <daniel.arbesu.sanchez@gmail.com>
2022-11-09 23:49:43 +0100

    Add JS

--- Modulo-1/3-javascript/2-ejercicios/javascript-fundamentals/ejercicios/04-booleans/index.js
+++ Modulo-1/3-javascript/2-ejercicios/javascript-fundamentals/ejercicios/04-booleans/index.js
@@ -1,38 +1,38 @@
 // 1. Añade a las siguientes tres variables cualquier valor que se considere "truthy". Usa valores diferentes para cada caso.
-var firstTruthyValue;
-var secondTruthyValue;
-var thirdTruthyValue;
+var firstTruthyValue = true;
+var secondTruthyValue = 1;
+var thirdTruthyValue = "Hola";
 
 // 2. Añade a las siguientes tres variables cualquier valor que se considere "falsy". Usa valores diferentes para cada caso.
-var firstFalsyValue;
-var secondFalsyValue;
-var thirdFalsyValue;
+var firstFalsyValue = false;
+var secondFalsyValue = 0;
+var thirdFalsyValue = "";
 
 // 3. Dadas las siguientes variables responde, justo debajo de cada una, si dicha comparación sería "true" o "false".
 
 var firstComparison = (0 < 1);
-var firstAnswer;
+var firstAnswer = true;
 
 var secondComparison = (0 == '0');
-var secondAnswer;
+var secondAnswer = true;
 
 var thirdComparison = (0 === '0');
-var thirdAnswer;
+var thirdAnswer = false;
 
 var fourthComparison = (0 != '0');
-var fourthAnswer;
+var fourthAnswer = false;
 
 var fifthComparison = (0 !== '0');
-var fifthAnswer;
+var fifthAnswer = true;
 
 var sixthComparison = (0);
-var sixthAnswer;
+var sixthAnswer = false;
 
 var seventhComparison = ('0');
-var seventhAnswer;
+var seventhAnswer = true;
 
 var eigththComparison = (6 >= '6');
-var eigththAnswer;
+var eigththAnswer = true;
 
 
 // ///////////////////////////////////////////////////////////////
