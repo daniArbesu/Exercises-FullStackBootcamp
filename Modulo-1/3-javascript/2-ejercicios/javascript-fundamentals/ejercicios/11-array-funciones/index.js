d8b72cea9bb07bb5efa74c4289d052687d42053d
daniArbesu <daniel.arbesu.sanchez@gmail.com>
2022-11-11 17:02:56 +0100

    Add JS to comply with exercise 11

--- Modulo-1/3-javascript/2-ejercicios/javascript-fundamentals/ejercicios/11-array-funciones/index.js
+++ Modulo-1/3-javascript/2-ejercicios/javascript-fundamentals/ejercicios/11-array-funciones/index.js
@@ -2,8 +2,11 @@
 // un string con otra fruta nueva, y luego retorne otro array igual (pero con
 // otra referencia) pero habiéndole añadido en la primera posición la fruta nueva.
 
+const getNewArray = (fruits, newFruit) => fruits.slice().unshift(newFruit);
+
 // 2. Crea la función "modifyOriginalArray" que reciba un array de números o strings,
 // le elimine la primera y la última posición y lo retorne (misma referencia)
+const modifyOriginalArray = (array) => array.shift().pop();
 
 // --EXTRA-- Dado el array "colors", crea la función "getColors" que reciba un número
 // como parámetro, cree una copia, y añada el color "black" en la última posición de
@@ -11,6 +14,12 @@
 // si es impar, y luego retorne dicho array
 const colors = ["blue", "red", "gray"];
 
+const getColors = (num) => {
+    const newColors = colors.slice();
+
+    num % 2 === 0 ? newColors.push("black") : newColors.unshift("white");
+    return newColors;
+  }
 // ///////////////////////////////////////////////////////////////
 // ⬇️ NO TOCAR ESTA PARTE O ROMPERÁS LOS TESTS ⬇️
 export default { getNewArray, modifyOriginalArray, colors, getColors };
