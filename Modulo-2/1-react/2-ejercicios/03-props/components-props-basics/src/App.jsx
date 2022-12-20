437ea70f5feb5016af92dffa727942aeefdc2c92
daniArbesu <daniel.arbesu.sanchez@gmail.com>
2022-12-20 22:59:21 +0100

    feat: add index.js to create a better structure

--- Modulo-2/1-react/2-ejercicios/03-props/components-props-basics/src/App.jsx
+++ Modulo-2/1-react/2-ejercicios/03-props/components-props-basics/src/App.jsx
@@ -1,8 +1,5 @@
 import './App.css';
-import Subtitle from './components/Subtitle';
-import Title from './components/Title';
-import Image from './components/Image';
-import Paragraph from './components/Paragraph';
+import { Image, Paragraph, Subtitle, Title } from './components';
 
 function App() {
   return (
---
+++ Modulo-2/1-react/2-ejercicios/03-props/components-props-basics/src/components/index.js
@@ -0,0 +1,6 @@
+import Image from './Image/Image';
+import Paragraph from './Paragraph/Paragraph';
+import Subtitle from './Subtitle/Subtitle';
+import Title from './Title/Title';
+
+export { Image, Paragraph, Subtitle, Title };
