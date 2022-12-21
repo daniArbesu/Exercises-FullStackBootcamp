363c603bf64300e5e6396a8773058033f8694035
daniArbesu <daniel.arbesu.sanchez@gmail.com>
2022-12-21 20:06:47 +0100

    feat: add jsx to comply with exercise 1

---
+++ Modulo-2/1-react/2-ejercicios/general/01-jsx-interpolacion/src/App.jsx
@@ -0,0 +1,33 @@
+import './App.css';
+
+const school = 'Rock{theCode}';
+const chapters = 6;
+const isAwesome = true;
+const technologies = ['JavaScript', 'React', 'Node'];
+const profile = {
+  job: 'Web developer',
+  description: '¡Me encanta la programación y el desarrollo web!',
+};
+
+function App() {
+  return (
+    <div className="App">
+      <h1> Estudio en {school}</h1>
+      <p>Veremos un total de {chapters} módulos de contenido</p>
+      <p>
+        Lo que estoy aprendiendo en {school} está siendo{' '}
+        {isAwesome ? 'increible' : 'top'}
+      </p>
+      <ul>
+        {technologies.map((technology) => (
+          <li>Aprendemos a usar {technology}</li>
+        ))}
+      </ul>
+      <h3>
+        Quiero ser {profile.job} y {profile.description}
+      </h3>
+    </div>
+  );
+}
+
+export default App;
