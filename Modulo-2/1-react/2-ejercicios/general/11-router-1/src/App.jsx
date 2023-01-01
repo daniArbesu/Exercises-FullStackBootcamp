cab59a2362b5f10007cd5ac27eea1e52c6c3ea14
daniArbesu <daniel.arbesu.sanchez@gmail.com>
2023-01-01 20:04:15 +0100

    fix: move BrowserRouter to main component

--- Modulo-2/1-react/2-ejercicios/general/11-router-1/src/App.jsx
+++ Modulo-2/1-react/2-ejercicios/general/11-router-1/src/App.jsx
@@ -6,16 +6,14 @@ import { BrowserRouter, Route, Link, Routes } from 'react-router-dom';
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
-        <nav>
-          <Link to="/">Inicio | </Link>
-          <Link to="/about">About Me | </Link>
-        </nav>
-        <Routes>
-          <Route exact path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-        </Routes>
-      </BrowserRouter>
+      <nav>
+        <Link to="/">Home | </Link>
+        <Link to="/about">About Me | </Link>
+      </nav>
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
     </div>
   );
 }
