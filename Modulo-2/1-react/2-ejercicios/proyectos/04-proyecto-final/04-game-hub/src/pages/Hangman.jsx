26201005be5ea5137365cfe4946f2a05ec5c7796
daniArbesu <daniel.arbesu.sanchez@gmail.com>
2023-01-14 12:35:38 +0100

    fix: fix import of components

--- Modulo-2/1-react/2-ejercicios/proyectos/04-proyecto-final/04-game-hub/src/pages/Hangman.jsx
+++ Modulo-2/1-react/2-ejercicios/proyectos/04-proyecto-final/04-game-hub/src/pages/Hangman.jsx
@@ -1,5 +1,5 @@
-import React, { useState, useEffect } from 'react';
-import HangmanBoard from './components/HangmanBoard';
+import React, { useState } from 'react';
+import HangmanBoard from '../components/HangmanBoard';
 
 const Hangman = () => {
   const [isStarted, setIsStarted] = useState(false);
